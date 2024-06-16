91eba26e09b3fc6a7727403153f61f7fba533cdd
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-16 22:10:31 +0200

    feat: add file upload functionality to the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
 export default function Home() {
   return (
     <main className="flex lg:flex-col justify-center items-center gap-8 m-auto max-w-4xl min-h-full">
-      <div className="">card</div>
+      <div className="bg-indigo-100 shadow-xl w-96 card">
+        <div className="card-body">
+          <label className="form-control w-full max-w-xs">
+            <span className="label-text"> </span>
+            <input
+              type="file"
+              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+            />
+            <div className="label"></div>
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <span className="label-text"> </span>
+            <input
+              type="file"
+              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+            />
+            <div className="label"></div>
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <span className="label-text"> </span>
+            <input
+              type="file"
+              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+            />
+            <div className="label"></div>
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <span className="label-text"> </span>
+            <input
+              type="file"
+              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+            />
+            <div className="label"></div>
+          </label>
+          {/* <div className="justify-start card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div> */}
+        </div>
+      </div>
       <div className="">preview</div>
     </main>
   );
