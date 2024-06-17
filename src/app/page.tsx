6992443bc32e3250e4152678eeb780fb7d16affe
Mirko Bozzetto <mirkouch@gmail.com>
2024-06-17 11:57:41 +0200

    chore: Update Home page layout and add file upload functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import { FileChangeEvent } from "@/types/types";
+import { useState } from "react";
+
 export default function Home() {
+  const [settings, setSettings] = useState();
+  const [image, setImage] = useState();
+
+  const handleFileChange = (e: FileChangeEvent) => {
+    const file = e.target.files?.[0];
+    console.log(e.target.files?.[0]);
+  };
+
   return (
-    <main className="flex lg:flex-col justify-center items-center gap-8 m-auto max-w-4xl min-h-full">
+    <main className="flex lg:flex-row flex-col justify-center items-center gap-8 m-auto max-w-4xl min-h-full">
       <div className="bg-indigo-100 shadow-xl w-96 card">
         <div className="card-body">
           <label className="form-control w-full max-w-xs">
@@ -8,42 +21,45 @@ export default function Home() {
             <input
               type="file"
               className="w-full max-w-xs file-input file-input-primary file-input-sm"
+              onChange={handleFileChange}
             />
-            <div className="label"></div>
           </label>
 
           <label className="form-control w-full max-w-xs">
-            <span className="label-text"> </span>
+            <span className="mb-1 label-text">Padding</span>
             <input
-              type="file"
-              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+              type="range"
+              min={0}
+              max="100"
+              value="0"
+              className="range range-primary range-sm"
             />
-            <div className="label"></div>
           </label>
 
           <label className="form-control w-full max-w-xs">
-            <span className="label-text"> </span>
+            <span className="mb-1 label-text">Shadow</span>
             <input
-              type="file"
-              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+              type="range"
+              min={0}
+              max="100"
+              value="0"
+              className="range range-primary range-sm"
             />
-            <div className="label"></div>
           </label>
 
           <label className="form-control w-full max-w-xs">
-            <span className="label-text"> </span>
+            <span className="mb-1 label-text">Radius</span>
             <input
-              type="file"
-              className="w-full max-w-xs file-input file-input-primary file-input-sm"
+              type="range"
+              min={0}
+              max="360"
+              value="0"
+              className="range range-primary range-sm"
             />
-            <div className="label"></div>
           </label>
-          {/* <div className="justify-start card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div> */}
         </div>
       </div>
-      <div className="">preview</div>
+      <div className="ml-12 w-96 card">preview</div>
     </main>
   );
 }
